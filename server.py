d0b897872bab324c4d16bc6c0abf6e7defe159ff
unknown <woshiallenyang@hotmail.com>
2020-09-23 13:03:49 -0400

    9/23/2020 1:03PM

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+# todos:
+# remove the player from game session after he disconnect
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, join_room, leave_room
 from random import seed, random
@@ -10,63 +12,170 @@ app.config['SECRET_KEY'] = "testflaskapp"
 socketio = SocketIO(app)
 
 users = {}
+roomStates = {}
+chessColor = ['White', 'Black']
+# modes for checking five in a row 
+checkMode = [
+	[1,0],#Horizontal
+	[0,1],#Vertical
+	[1,1],#Left diagonal
+	[1,-1],#Right diagonal
+]
 
 def createNewName(id):
-    return 'USER'+id[:8]
+	return 'USER'+id[:8]
+
+def checkUserAvailability(id):
+	for room in roomStates.values():
+		if room['player1'] == id or room['player2'] == id:
+			return False
+	return True
+
+# Check if there are already 5 of a color in a row
+def checkWin(x, y, color, room, mode):
+	connectCount = 1
+	board = room['boardState']
+
+	# count the consecutive stones on the increasing side
+	for i in range(1,5):
+		if(board[x + i*mode[0]]):
+			if(board[x + i*mode[0]][y + i*mode[1]] == color):
+				connectCount += 1
+			else:
+				break
+
+	# count the consecutive stones on the decreasing side
+	for j in range(1,5):
+		if(board[x - j*mode[0]]):
+			if(board[x - j*mode[0]][y - j*mode[1]] == color):
+				connectCount += 1
+			else:
+				break
+   
+	if(connectCount >= 5):
+		print("{} has won".format(color))
+		socketio.emit('game end', color, room=room['player1'])
+		socketio.emit('game end', color, room=room['player2'])
 
 @app.route('/')
 def sessions():
-    return render_template('game.html')
+	return render_template('game.html')
 
 @socketio.on('connect')
 def connectHandler():
-    sessionId = request.sid
-    username = 'user'+sessionId[:8]
-    userInfo = {
-        "id": sessionId,
-        "name":username,
-        "against": '',
-        "current": False,
-        "isBlack": False
-    }
-    users[sessionId] = userInfo
-    print('User: {} has joined'.format(username))
-    socketio.emit('userList', users, boradcast=True)
+	sessionId = request.sid
+	username = 'user'+sessionId[:8]
+	userInfo = {
+		"id": sessionId,
+		"name":username,
+		"against": '',
+		"current": False,
+		"isBlack": False
+	}
+	users[sessionId] = userInfo
+	print('User: {} has joined'.format(username))
+	socketio.emit('userList', users, boradcast=True)
 
 @socketio.on('disconnect')
 def disconnectHandler():
-    sessionId = request.sid
-    username = 'user'+sessionId[:8]
-    users.pop(sessionId)
-    print('User: {} has left'.format(username))
-    socketio.emit('userList', users, boradcast=True)
-    
+	sessionId = request.sid
+	username = 'user'+sessionId[:8]
+	users.pop(sessionId)
+	print('User: {} has left'.format(username))
+	socketio.emit('userList', users, boradcast=True)
+	rooms = []
+	for roomId, room in roomStates.items():
+		if room['player1'] == sessionId or room['player2'] == sessionId:
+			rooms.append(roomId)
+	for id in rooms:
+		roomStates.pop(id)
+	
 @socketio.on('inviteGame')
-def inviteGameHandler(opponentId, methods=['GET', 'POST']):
-    inviteId = request.sid
-    users[inviteId]['against'] = opponentId
-    users[opponentId]['against'] = inviteId
-    # Coin toss
-    seed()
-    if random() > 0.5:
-        users[inviteId]['current'] = True
-        users[inviteId]['isBlack'] = True
-    else:
-        users[opponentId]['current'] = True
-        users[opponentId]['isBlack'] = True
-    
-    print("Start game between {} and {}".format(inviteId, opponentId))
-    socketio.emit('beginGame', users[inviteId], room=inviteId)
-    socketio.emit('beginGame', users[opponentId], room=opponentId)
-    socketio.emit('userList', users, broadcast=True)
+def inviteGameHandler(json, methods=['GET', 'POST']):
+	inviterId = request.sid
+	opponentId = json['id']
 
-def messageReceived(methods=['GET', 'POST']):
-    print('message was received!!!')
+	# if this is a new match
+	if not json['rematch']:
+		isInviterAvailable = checkUserAvailability(inviterId)
+		isOpponentAvailable = checkUserAvailability(opponentId)
 
-@socketio.on('stone placed')
+		if isInviterAvailable and isOpponentAvailable:
+			seed()
+			gameId = int(random()*1000)
+			roomStates[gameId] = {}
+			roomStates[gameId]['player1'] = inviterId
+			roomStates[gameId]['player2'] = opponentId
+			roomStates[gameId]['stepCount'] = 0
+			roomStates[gameId]['boardState'] = [[0 for x in range(19)] for y in range(19)] 
+			
+			users[inviterId]['against'] = opponentId
+			users[inviterId]['gameId'] = gameId
+			users[opponentId]['against'] = inviterId
+			users[opponentId]['gameId'] = gameId
+			# Coin toss
+			if gameId%2 == 1:
+				# users[inviteId]['current'] = True
+				users[inviterId]['isBlack'] = True
+				roomStates[gameId]['currentPlayer'] = inviterId
+			else:
+				# users[opponentId]['current'] = True
+				users[opponentId]['isBlack'] = True
+				roomStates[gameId]['currentPlayer'] = opponentId
+			
+			print("Start game between {} and {} in room {}".format(inviterId, opponentId, gameId))
+			socketio.emit('beginGame', users[inviterId], room=inviterId)
+			socketio.emit('beginGame', users[opponentId], room=opponentId)
+			socketio.emit('userList', users, broadcast=True)
+
+def isBoardPosValid(x, y, boardState):
+	i = x//30-1
+	j = y//30-1
+
+	if boardState[i][j] == 0:
+		return True
+	else:
+		return False
+
+@socketio.on('stone placement')
 def stonePlacedHandler(json, methods=['GET', 'POST']):
-    print('received a new placed stone: ' + str(json))
-    socketio.emit('stone placed', json, callback=messageReceived)
+	#print('received a new placed stone: ' + str(json))
+	
+	senderPlayerId = request.sid
+	receiverPlayerId = json['against']
+	gameId = users[senderPlayerId]['gameId']
+	senderIsBlack = users[senderPlayerId]['isBlack']
+
+	# if not sender's turn to play, reject the attempt
+	if roomStates[gameId]['currentPlayer'] != senderPlayerId:
+		print("Rejected attempt due to not his turn")
+		return
+	
+	# if the step count is not correct, reject the attempt
+	if json['step'] != roomStates[gameId]['stepCount']:
+		print("Rejected attempt due to wrong turn number")
+		return
+
+	if isBoardPosValid(json['x'], json['y'], roomStates[gameId]['boardState']):
+		i = json['x']//30-1
+		j = json['y']//30-1
+		roomStates[gameId]['boardState'][i][j] = chessColor[senderIsBlack]
+		roomStates[gameId]['stepCount'] += 1
+		roomStates[gameId]['currentPlayer'] = receiverPlayerId
+		print("{} stone palced at [{}, {}]".format(chessColor[senderIsBlack], i, j))
+		#print(roomStates[gameId])
+
+		# the stone placement attempt is verified by the server, 
+		# the players can update their board
+		socketio.emit('stone placement confirm', json, room=senderPlayerId)
+		socketio.emit('stone placement confirm', json, room=receiverPlayerId)
+		print("stone placement confirmed")
+
+		for modeIndex in range(4):
+			checkWin(i, j, chessColor[senderIsBlack], roomStates[gameId], checkMode[modeIndex])
+	# if board position not valid, reject the attempt
+	else:
+		return
 
 if __name__ == '__main__':
-    socketio.run(app)
+	socketio.run(app)
--- static/js/game.js
+++ static/js/game.js
@@ -1,238 +1,291 @@
 // Connect to the server socket
 var socket = io.connect('http://127.0.0.1:5000/',
-                        );
+						);
 
 // The client keeps a list of all connected clients except self
 var users = {};
 var isBlack = true;
 var PLAYER_USERNAME = "";
 var OPPONENT_USERNAME = "";
+var OPPONENT_ID = "";
 
-// When the socket connects, start the game
-socket.on('connect', function() {
-    PLAYER_USERNAME = "USER"+socket.id.substring(0,8);
-    prompt.text(`Welcome ${PLAYER_USERNAME}`);
-});
-
-socket.on('beginGame', (json) => {
-    console.log("Begin game", json);
-    OPPONENT_USERNAME = users[json.against].name;
-    if (json.current) {
-        prompt.text("Your turn");
-    }
-    else {
-        prompt.text(`Waiting for ${OPPONENT_USERNAME} to place a stone`);
-    }
-    startGame();
-});
-
-// Upon receiving response from the server, display the message
-socket.on('stone placed', function(json) {
-	console.log(json);
-})
-
-socket.on('userList', function(json) {
-    //console.log(json);
-    users = {};
-    $.each(json, function(key, value) {
-        if (key != socket.id) {
-            users[key] = value;
-        }
-    });
-    updateDOMUserList(users, socket.id, socket);
-})
-
-var canvas = $("#chessboard")[0];
-var prompt = $("#prompt");
-var context = canvas.getContext("2d");
-
-//An array that records the stones on the chessboard
-var board = [];
-
-var chessColor = ["black", "white"];
+// Game state info
+var board = []; //An array that records the stones on the chessboard
+var isCurrent = false;
 var stepCount = 0;
 var hasWon = false;
 
-// modes for checking five in a row 
-var checkMode = [
-    [1,0],//Horizontal
-    [0,1],//Vertical
-    [1,1],//Left diagonal
-    [1,-1],//Right diagonal
-];
+var canvas = $("#chessboard")[0];
+var prompt = $("#prompt");
+var debug = $("#debug");
+var context = canvas.getContext("2d");
+var chessColors = ["White", "Black"];
 
 function startGame() {
-    // Initialize the board state to 0s
-    for(var i=0; i<19; i++){
-       board[i] = [];
-       for(var j=0; j<19; j++){
-          board[i][j] = 0;
-        }    
-    }
-    cleanChessBoard();
-    drawChessBoard();
-    hasWon = false;
+	cleanChessBoard();
+	cleanState();
+	drawChessBoard();
 }
 
 // Draw out the lines on the chessboard
 function drawChessBoard() {
-    for (var i = 0; i < 19; i++) {
-        context.beginPath();
-        context.moveTo((i+1) * 30, 30);
-        context.lineTo((i+1) * 30, canvas.height - 30);
-        context.closePath();
-        context.stroke();
-        context.beginPath();
-        context.moveTo(30, (i+1) * 30);
-        context.lineTo(canvas.width - 30, (i+1) * 30);
-        context.closePath();
-        context.stroke();
-    }
+	for (var i = 0; i < 19; i++) {
+		context.beginPath();
+		context.moveTo((i+1) * 30, 30);
+		context.lineTo((i+1) * 30, canvas.height - 30);
+		context.closePath();
+		context.stroke();
+		context.beginPath();
+		context.moveTo(30, (i+1) * 30);
+		context.lineTo(canvas.width - 30, (i+1) * 30);
+		context.closePath();
+		context.stroke();
+	}
 }
 
 // Clean all the stones on the chessboard
 function cleanChessBoard() {
-    context.fillStyle = "white";
-    context.fillRect(0, 0, canvas.width, canvas.height);
-    stepCount = 0;
+	context.fillStyle = "white";
+	context.fillRect(0, 0, canvas.width, canvas.height);
+	stepCount = 0;
+}
+
+function cleanState() {
+	isBlack = true;
+	PLAYER_USERNAME = "";
+	OPPONENT_USERNAME = "";
+	OPPONENT_ID = "";
+
+	// Game state info
+	// Initialize the board state to 0s
+	for(var i=0; i<19; i++){
+	   board[i] = [];
+	   for(var j=0; j<19; j++){
+		  board[i][j] = 0;
+		}    
+	}
+	isCurrent = false;
+	stepCount = 0;
+	hasWon = false;
 }
 
 // Draw the stone at position (x, y) and fill it with color
 function drawStone(x, y, color) {
-    context.beginPath();
-    context.arc(x, y, 13, 0, Math.PI*2, false);
-    context.closePath();
-    context.fillStyle = color;
-    context.fill();
-    context.lineWidth = 1;
-    context.strokeStyle = 'black';
-    context.stroke();
+	context.beginPath();
+	context.arc(x, y, 13, 0, Math.PI*2, false);
+	context.closePath();
+	context.fillStyle = color;
+	context.fill();
+	context.lineWidth = 1;
+	context.strokeStyle = 'black';
+	context.stroke();
 }
 
 // Check if the stone location is valid
 function isBoardPosValid(x, y) {
-    let i = x/30-1; // chessMap horizontal index
-    let j = y/30-1; // chessMap vertical index
+	let i = x/30-1; // chessMap horizontal index
+	let j = y/30-1; // chessMap vertical index
 
-    if (board[i][j] == 0) {
-        return true;
-    }
-    else {
-        return false;
-    }
+	if (board[i][j] == 0) {
+		return true;
+	}
+	else {
+		return false;
+	}
 }
 
 // Event listener fir stone placement
 canvas.addEventListener("click", function(e) {
-    if (hasWon) {
-        alert("Game Over~");
-        return;
-    }
+	if (hasWon) {
+		alert("Game Over~");
+		return;
+	}
 
-    // Check if the player clicked out of boundary
-    if(e.offsetX < 15 || e.offsetX > 585 || e.offsetY < 15 || e.offsetY > 585) {
-       return;
-    }
+	if (!isCurrent) {
+		alert("Wait for your turn");
+		return;
+	}
 
-    // Round the stone position to the closest line cross
-    let x = Math.floor((e.offsetX + 15) / 30 ) * 30;
-    let y = Math.floor((e.offsetY + 15) / 30 ) * 30;
+	// Check if the player clicked out of boundary
+	if(e.offsetX < 15 || e.offsetX > 585 || e.offsetY < 15 || e.offsetY > 585) {
+	   return;
+	}
 
-    // Check if the clicked stone location is valid
-    let i = x/30-1; // chessMap horizontal index
-    let j = y/30-1; // chessMap vertical index
+	// Round the stone position to the closest line cross
+	let x = Math.floor((e.offsetX + 15) / 30 ) * 30;
+	let y = Math.floor((e.offsetY + 15) / 30 ) * 30;
 
-    if(isBoardPosValid(x, y)) {
-        drawStone(x, y, chessColor[stepCount % 2]);
-        board[i][j] = chessColor[stepCount % 2];
-        //检查当前玩家是否赢了游戏
-        for(var index=0; index<4; index++) {
-            checkWin(i, j, chessColor[stepCount % 2], checkMode[index]);
-        }
+	// Check if the clicked stone location is valid
+	let i = x/30-1; // chessMap horizontal index
+	let j = y/30-1; // chessMap vertical index
 
-        socket.emit('stone placed', {
-            x: x,
-            y: y,
-            color: chessColor[stepCount % 2],
-            step: stepCount
-        });
-
-        stepCount++;
-        prompt.text("Current: "+chessColor[(stepCount) % 2]);
-    } 
+	if(isBoardPosValid(x, y)) {
+		socket.emit('stone placement', {
+			placedBy: socket.id,
+			x: x,
+			y: y,
+			step: stepCount,
+			against: OPPONENT_ID
+		});
+	} 
 });
 
 window.addEventListener('beforeunload', function(e) {
-    socket.disconnect();
+	socket.disconnect();
 });
 
-// Check if there are already 5 of a color in a row
-function checkWin(x, y, color, mode) {
-    let count = 1;
-    for(var i=1; i<5; i++) {
-        if(board[x + i * mode[0]]){
-            if(board[x + i * mode[0]][y + i * mode[1]] == color){
-                count++;
-            }else{
-                break;
-            }
-        }
-    }
-    
-    for(var j=1;j<5;j++){
-        if(board[x - j * mode[0]]){
-            if(board[x - j * mode[0]][y - j * mode[1]] == color){
-                count++;
-            }else{
-                break;
-            }
-        }
-    }
-    // if(mode == checkMode[0])
-    // { console.log("水平方向有： " + count + "个" + color);}
-    // if(mode == checkMode[1])
-    // { console.log("竖直方向有： " + count + "个" + color);}
-    // if(mode == checkMode[2])
-    // { console.log("左斜方向有： " + count + "个" + color);}
-    // if(mode == checkMode[3])
-    // { console.log("右斜方向有： " + count + "个" + color);}
-   
-    if(count >= 5){
-        alert(color + " you have win!");
-        // 游戏结束
-        hasWon = true;
-    }
-}
-
 function bindInviteButtonClick(socket) {
-    $('.user-status').click(function (e) {
-        socket.emit('inviteGame', $(this).data('id'));
-        OPPONENT_USERNAME = $(this).data('name');
-    });
+	$('.user-status').click(function (e) {
+		// if the invited player is not available, reject 
+		if ($(this).html() != "Invite") {
+			alert("This player is not available, try later");
+			return;
+		}
+
+		// if you are already in a game, reject
+		if (OPPONENT_USERNAME != "") {
+			alert("Finish your game first");
+			return;
+		}
+
+		socket.emit('inviteGame', {
+			id: $(this).data('id'),
+			rematch: false
+		});
+
+		OPPONENT_USERNAME = $(this).data('name');
+		console.log('sent');
+	});
 }
 
 function updateDOMUserList(userList, ownId, socket) {
-    var result = '';
-    $.each(userList, function (index, value) {
-        value.statusClass = '';
-        value.statusText = 'Invite';
+	var result = '';
+	$.each(userList, function (index, value) {
+		value.statusClass = '';
+		value.statusText = 'Invite';
 
-        if (value.against) {
-            value.statusClass = 'in-game';
-            value.statusText = 'In-Game';
-        }
-        if (value.against === ownId) {
-            value.statusClass = 'game-with';
-            value.statusText = 'Opponent';
-            OPPONENT_USERNAME = value.name;
-        }
-        result += `
-            <div class="user-item row align-items-center">
-                <p class="user-info col-8">${value.name}</p>
-                <button class="user-status col-4 ${value.statusClass}" data-id=${value.id} data-name=${value.name}>${value.statusText}</button>
-            </div>
-        `
-    });
-    $('#user-list').html(result);
-    bindInviteButtonClick(socket);
+		if (value.against) {
+			value.statusClass = 'in-game';
+			value.statusText = 'In-Game';
+		}
+		if (value.against === ownId) {
+			value.statusClass = 'game-with';
+			value.statusText = 'Opponent';
+			OPPONENT_USERNAME = value.name;
+		}
+		result += `
+			<div class="user-item row align-items-center">
+				<p class="user-info col-8">${value.name}</p>
+				<button class="user-status col-4 ${value.statusClass}" data-id=${value.id} data-name=${value.name}>${value.statusText}</button>
+			</div>
+		`
+	});
+	$('#user-list').html(result);
+	bindInviteButtonClick(socket);
 }
+
+function updatePrompt() {
+	if (isCurrent) {
+		prompt.text("Your turn");
+	}
+	else {
+		prompt.text(`Waiting for ${OPPONENT_USERNAME} to place a stone`);
+	}
+}
+
+function updateDebugInfo() {
+	result = ""
+	result += `isCurrent: ${isCurrent}<br>`;
+	result += `isBlack: ${isBlack}<br>`;
+	result += `ID: ${socket.id}<br>`;
+	result += `OPPONENT_ID: ${OPPONENT_ID}<br>`;
+	result += `OPPONENT_USERNAME: ${OPPONENT_USERNAME}<br>`;
+	result += `stepCount: ${stepCount}`;
+	debug.html(result);
+}
+
+
+// Socket event handling
+
+// When the socket connects, start the game
+socket.on('connect', function() {
+	PLAYER_USERNAME = "USER"+socket.id.substring(0,8);
+	prompt.text(`Welcome ${PLAYER_USERNAME}`);
+	updateDebugInfo();
+});
+
+socket.on('beginGame', (json) => {
+	startGame();
+	console.log("Begin game", json);
+	OPPONENT_ID = users[json.against].id
+	OPPONENT_USERNAME = users[json.against].name;
+	isCurrent = json.isBlack;
+	isBlack = json.isBlack;
+	updatePrompt();
+	updateDebugInfo();
+});
+
+// Upon receiving the new user list, update the DOM
+socket.on('userList', function(json) {
+	//console.log(json);
+	users = {};
+
+	// Excluding self from the user list
+	$.each(json, function(key, value) {
+		if (key != socket.id) {
+			users[key] = value;
+		}
+	});
+	updateDOMUserList(users, socket.id, socket);
+});
+
+socket.on('stone placement confirm', function(json) {
+	let chessColor = "";
+	x = json.x;
+	y = json.y;
+
+	if (socket.id == json.placedBy) {
+		chessColor = chessColors[+isBlack];
+		isCurrent = false;
+	}
+	else {
+		chessColor = chessColors[+(!isBlack)];
+		isCurrent = true;
+	}
+
+	let i = x/30-1; // chessMap horizontal index
+	let j = y/30-1; // chessMap vertical index
+
+	drawStone(x, y, chessColor);
+	board[i][j] = chessColor;
+	
+	// for(var index=0; index<4; index++) {
+	// 	checkWin(i, j, chessColor, checkMode[index]);
+	// }
+
+	stepCount++;
+	updatePrompt();
+});
+
+socket.on('game end', function(data) {
+	let winner = data;
+	let chessColor = chessColors[+isBlack];
+	
+
+	if (winner == chessColor) {
+		if (confirm("You have won, play it again?")) {
+			console.log(winner, chessColor);
+		}
+		else {
+			return;
+		}
+	}
+	else {
+		if (confirm("You have lost, play it again?")) {
+			console.log(winner, chessColor);
+		}
+		else {
+			return;
+		}
+	}
+});
